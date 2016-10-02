457eb013fe6a9611582407a2ddac6f266ca067ec
Briken <brokenbriken@gmail.com>
2016-10-02 15:10:28 +0100

    XMLSerialize Script added
    Menu scene and xml serialise scripts added

---
+++ Assets/Code/TestDataClass.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class TestDataClass
+{
+
+    public string name;
+    public string description;
+
+    public TestDataClass(string s1, string s2)
+    {
+        name = s1;
+        description = s2;
+    }
+}
---
+++ Assets/Code/XMLScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using UnityEngine.SceneManagement;
+
+public class XMLScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SaveGame()
+    {
+        TestDataClass tdc = new TestDataClass("My Name", "This is a serialized item");
+        XmlSerializer x = new XmlSerializer(tdc.GetType());
+
+        System.IO.FileStream file =
+            System.IO.File.Create("Save_File.xml");
+
+        x.Serialize(file, tdc);
+    }
+
+    public void LoadGame()
+    {
+        TestDataClass tdc = new TestDataClass("","");
+        XmlSerializer x = new XmlSerializer(tdc.GetType());
+
+        System.IO.FileStream file =
+            System.IO.File.OpenRead("Save_File.xml");
+        tdc = (TestDataClass)x.Deserialize(file);
+
+        file.Close();
+        print(tdc.name + " : " + tdc.description);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("<Level1Here>",  LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("<level1Here>"));
+    }
+}
