365bb2225e26f5d3fd90f147aa689972ac89c29c
Briken <brokenbriken@gmail.com>
2016-10-02 16:21:08 +0100

    Minimum Bug Implementation
    added in a couple of bugs, gonna finish the crash bug once ive worked
    out how to xml

--- Assets/Code/Bugs.cs
+++ Assets/Code/Bugs.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Bugs : MonoBehaviour {
 
+    public GameObject player;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +14,22 @@ public class Bugs : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
+    public void ReverseMouse(bool pop)
+    {
+        player.GetComponent<vp_FPInput>().MouseLookInvert = pop;
+    }
+
+    void CrashGame()
+    {
+        // on specific event required
+        Crash();
+    }
+
+    IEnumerator Crash(  )
+    {
+        yield return new WaitForSeconds(2); //Animation length of the crash animation for the wait time. jsut set to a randome number atm
+        Application.Quit();
+    }
 }
